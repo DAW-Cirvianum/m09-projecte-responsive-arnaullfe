aa2e3c853b4699dc07f279e4aec370685f294914
arnaullfe <arnaullfe@gmail.com>
2020-12-06 12:59:46 +0100

    millorada

--- resources/js/server_list.js
+++ resources/js/server_list.js
@@ -1,20 +1,32 @@
 var list = [];
-list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',photo:'../images/servers/gamers/minecraft.png'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
 
 
 var html = '';
 list.forEach((element)=>{
     html +=  `
-    <div class="col-sm-6 col-lg-4 mb-3">
+    <div class="col-sm-6 col-lg-4 mt-5">
     <div class="card mb-3 h-100">
       <img src="${element.photo}" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">${element.title}</h5>
+            <h5 class="card-title font-weight-bold">${element.title}</h5>
             <p class="card-text">${element.description}</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <p class="card-text"><small class="text-muted">${element.adds}</small></p>
+            <button class="btn btn-success btn-block">Detalls</button>
         </div>
     </div>
 </div>
-    
     `
 })
+
+document.getElementById("row-servers").innerHTML = html;
