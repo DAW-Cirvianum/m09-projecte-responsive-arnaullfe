42ce7d899ee51e6dfee0e9263df22460c8a791e4
arnaullfe <arnaullfe@gmail.com>
2020-12-08 17:33:28 +0100

    nous sv

--- resources/js/server_list.js
+++ resources/js/server_list.js
@@ -1,15 +1,15 @@
 var list = [];
-list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
-list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
-list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
-list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
-list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
-list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
-list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
-list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
-list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
-list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
-list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/gamers/minecraft.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/minecraft.jpg'});
+list.push({title:'Servidor PHP',description:'Servidor php per pujar els teus ptojectes',adds:"Versió 7.1 de PHP",photo:'../resources/images/servers/php.png'});
+list.push({title:'Servidor SQL',description:'Servidor sql per realitzar operacions sql',adds:"Versió 12.0 de SQL",photo:'../resources/images/servers/sql.jpg'});
+list.push({title:'Servidor Rocket League',description:'Servidor de rocket league de 2GB',adds:"Incorpora la de steam os 3.5",photo:'../resources/images/servers/rocket-league.png'});
+list.push({title:'Servidor League of Legends',description:'Servidor de League of legends de 4GB',adds:"Incorpora la versió 10.2",photo:'../resources/images/servers/lol.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/minecraft.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/minecraft.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/minecraft.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/minecraft.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/minecraft.jpg'});
+list.push({title:'Servidor Minecraft',description:'Servidor de minecraft de 4GB',adds:"Incorpora la versió 3.2",photo:'../resources/images/servers/minecraft.jpg'});
 
 
 var html = '';
@@ -22,7 +22,7 @@ list.forEach((element)=>{
             <h5 class="card-title font-weight-bold">${element.title}</h5>
             <p class="card-text">${element.description}</p>
             <p class="card-text"><small class="text-muted">${element.adds}</small></p>
-            <button class="btn btn-success btn-block">Detalls</button>
+            <button class="btn btn-success btn-block" onclick="location.href='./server_selected.html'">Detalls</button>
         </div>
     </div>
 </div>
