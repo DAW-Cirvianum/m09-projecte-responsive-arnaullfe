b4cbf0f4bd9cbb1b5db1dc041f0bd041abd1814f
Hector <hector.ortiz@cirvianum.cat>
2020-12-14 15:35:17 +0100

    tot ben posat

--- resources/js/server_list.js
+++ resources/js/server_list.js
@@ -17,7 +17,7 @@ list.forEach((element)=>{
     html +=  `
     <div class="col-sm-6 col-lg-4 mt-5">
     <div class="card mb-3 h-100">
-      <img src="${element.photo}" class="card-img-top" alt="...">
+      <img src="${element.photo}" class="card-img-top" alt="..." style="height:200px">
         <div class="card-body">
             <h5 class="card-title font-weight-bold">${element.title}</h5>
             <p class="card-text">${element.description}</p>
